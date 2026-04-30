90c8afe35f24546bb4fdf9a96907bc7c0eb179fb
Alexey Rybakov <rybakov.aax@gmail.com>
2026-04-30 11:55:54 +0300

    #23 : numbers

--- src/numbers/numbers.py
+++ src/numbers/numbers.py
@@ -5,43 +5,6 @@ Numbers functions.
 import sortedcontainers
 
 #===================================================================================================
-# Pythagorian triplets.
-#===================================================================================================
-
-def pythagorian_triplets(max_n, max_k):
-    """
-    Sorted set of pythagorian triplets.
-
-    a = k * (n^2 - m^2)
-    b = k * 2 * n * m
-    c = k * (n^2 + m^2)
-
-    Parameters
-    ----------
-    max_n : int
-        Maximum value of n.
-    max_k : int
-        Maximum value of k.
-
-    Returns
-    -------
-    sortedcontainers.SortedSet
-        Set of triplets.
-    """
-
-    s = sortedcontainers.SortedSet()
-
-    for k in range(1, max_k + 1):
-        for m in range(1, max_n + 1):
-            for n in range(m + 1, max_n + 1):
-                a = k * (n**2 - m**2)
-                b = 2 * k * n * m
-                c = k * (n**2 + m**2)
-                s.add((a, b, c))
-
-    return s
-
-#===================================================================================================
 # Calculations by module.
 #===================================================================================================
 
@@ -88,9 +51,83 @@ def unique_quadratic_residues(p, is_0_included=True):
     return sortedcontainers.SortedSet(quadratic_residues(p, is_0_included))
 
 #===================================================================================================
+# Pythagorean triplets.
+#===================================================================================================
+
+def pythagorean_triplets(max_n, max_k):
+    """
+    Sorted set of pythagorean triplets.
+
+    a = k * (n^2 - m^2)
+    b = k * 2 * n * m
+    c = k * (n^2 + m^2)
+
+    Parameters
+    ----------
+    max_n : int
+        Maximum value of n.
+    max_k : int
+        Maximum value of k.
+
+    Returns
+    -------
+    sortedcontainers.SortedSet
+        Set of triplets.
+    """
+
+    s = sortedcontainers.SortedSet()
+
+    for k in range(1, max_k + 1):
+        for m in range(1, max_n + 1):
+            for n in range(m + 1, max_n + 1):
+                a = k * (n**2 - m**2)
+                b = 2 * k * n * m
+                c = k * (n**2 + m**2)
+                s.add((a, b, c))
+
+    return s
+
+#---------------------------------------------------------------------------------------------------
+
+def pythagorean_triplets_by_module(p):
+    """
+    Get all pythagorean triplets by module of p.
+
+    Parameters
+    ----------
+    p : int
+        Module.
+
+    Returns
+    -------
+    sortedcontainers.SortedSet
+        Set of triplets.
+    """
+
+    s = sortedcontainers.SortedSet()
+    r = range(p)
+
+    for v in r:
+        for u in range(v, p):
+            for k in r:
+                a = (k * (u**2 - v**2)) % p
+                b = (k * 2 * u * v) % p
+                c = (k * (u**2 + v**2)) % p
+                s.add((a, b, c))
+
+    return s
+
+#===================================================================================================
 
 if __name__ == '__main__':
-    print(unique_quadratic_residues(29, True))
-    print(pythagorian_triplets(2, 5))
+
+    # Calculations by module.
+    print('quadratic_residues(17, False) :', quadratic_residues(17, False))
+    print('unique_quadratic_residues(17, True) :',
+          unique_quadratic_residues(17, True))
+
+    # Pythagorean triplets.
+    print('pythagorean_triplets(2, 5) :', pythagorean_triplets(2, 5))
+    print('pythagorean_triplets_by_module(7) :', pythagorean_triplets_by_module(7))
 
 #===================================================================================================
